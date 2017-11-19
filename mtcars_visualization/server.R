3e3a8be4b3bf004c2517dd84cb4adf0f2deb708d
Dortje Löper <dortje@turboprinzessin.de>
2017-11-19 18:51:48 +0100

    added full server code to presentation

--- mtcars_visualization/server.R
+++ mtcars_visualization/server.R
@@ -9,39 +9,30 @@ mtcars$gear <- as.factor(mtcars$gear)
 mtcars$carb <- as.factor(mtcars$carb)
 
 shinyServer(function(input, output) {
-       
     lin_model <- reactive({
-        
         out <- input$select_out
         preds <- input$chkbox_pred
         preds <- preds[!preds %in% out]
         
         if(length(preds) > 0) {
-        
                 model_formula = formula(paste(out, "~", paste(preds, collapse="+")))
                 lm(model_formula, data=mtcars)
-            
         } else {
            return(NULL) 
         }
     })
-    
     output$var_plot <- renderPlot({
-
         x_var <- input$select_x
         y_var <- input$select_y
         col_var <- input$select_col
-
         if (col_var == "none") {
             g <- ggplot(mtcars, aes_string(x=x_var, y=y_var))
         } else {
             g <- ggplot(mtcars, aes_string(x=x_var, y=y_var, color=col_var))
         }
-
         g + geom_point() + geom_smooth(method="lm")
 
     })
-
     output$modelSummary <- renderPrint({
         if(!is.null(lin_model())) {
             summary(lin_model())
